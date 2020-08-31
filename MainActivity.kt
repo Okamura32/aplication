9eae9324490f627db925af5a932229d040c5d91f
Okamura32 <61731507+Okamura32@users.noreply.github.com>
2020-08-31 22:04:30 +0900

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,26 +1,68 @@
-package com.example.wan_cat
+package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
 
+//import java.util.Date
+
 class MainActivity : AppCompatActivity() {
+
+
+    val handler = Handler()
+    var timeValue = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var textMessage = findViewById<TextView>(R.id. text_message)
+        val timeText = findViewById(R.id.timeText) as TextView
+        val startButton = findViewById(R.id.start) as Button
+        val stopButton = findViewById(R.id.stop) as Button
+        val resetButton = findViewById(R.id.start) as Button
+//
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
 
-        var buttonCat = findViewById<Button>(R.id.button_cat)
-        var buttonDog = findViewById<Button>(R.id.button_dog)
-
-        buttonCat.setOnClickListener{
-            textMessage.setText(R.string.text_message_cat)
+                timeToText(timeValue)?.let {
+                    timeText.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
         }
 
-        buttonDog.setOnClickListener{
-            textMessage.setText(R.string.text_message_dog)
+        startButton.setOnClickListener {
+            handler.post(runnable)
+        }
+
+        stopButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+
+        resetButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = 0
+            timeToText()?.let {
+                timeText.text = it
+            }
+        }
+    }
+
+
+    private fun timeToText(time: Int = 0): String? {
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m, s)
         }
     }
 }
